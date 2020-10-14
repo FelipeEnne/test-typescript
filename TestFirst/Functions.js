191b6911af2c0d90c44b98b6f3ebc335cca8215a
FelipeEnne <felipeenne@gmail.com>
2020-10-14 20:29:57 -0300

    Interface

--- TestFirst/Functions.js
+++ TestFirst/Functions.js
@@ -16,3 +16,26 @@ function add3(num1, num2) {
 }
 console.log(add3(5));
 // add(5, '10');
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p = {
+    firstName: 'Bruce',
+    lastName: 'Wayne'
+};
+fullName(p);
+function fullName2(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p2 = {
+    firstName: 'Bruce',
+    lastName: 'Wayne Clone'
+};
+fullName2(p2);
+function fullName3(person) {
+    person.lastName !== undefined ? console.log(person.firstName + " " + person.lastName) : console.log("" + person.firstName);
+}
+var p3 = {
+    firstName: 'Batman'
+};
+fullName3(p3);
--- TestFirst/Functions.ts
+++ TestFirst/Functions.ts
@@ -19,3 +19,46 @@ function add3(num1: number, num2: number = 10): number {
 
 console.log(add3(5));
 // add(5, '10');
+
+
+function fullName(person : {firstName: string, lastName: string}) {
+  console.log(`${person.firstName} ${person.lastName}`)
+}
+
+let p = {
+  firstName: 'Bruce',
+  lastName: 'Wayne'
+}
+
+fullName(p);
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function fullName2(person : Person) {
+  console.log(`${person.firstName} ${person.lastName}`)
+}
+
+let p2 = {
+  firstName: 'Bruce',
+  lastName: 'Wayne Clone'
+}
+
+fullName2(p2);
+
+interface Person3 {
+  firstName: string;
+  lastName?: string;
+}
+
+function fullName3(person : Person3) {
+  person.lastName !== undefined ? console.log(`${person.firstName} ${person.lastName}`) : console.log(`${person.firstName}`);
+}
+
+let p3 = {
+  firstName: 'Batman',
+}
+
+fullName3(p3);
